f732c3d9236ce852b457d875ac58069672de4fd8
Your Name <you@example.com>
2017-06-27 22:28:30 +0300

    Реализация поставленной задачи

--- README.md
+++ README.md
@@ -6,9 +6,10 @@ This project helps to control time was spent on sites. It requires special exten
 
 Install extension for Chrome browser [Custom JavaScript for websites](https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija).
 
-Open configuration of [cjs](https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija) browser extension on the site you want to controll. Click on the link "your own external scripts", add path [TODO в качестве production хостинга файла с JavaScript подойдет GitHub - у каждого файла в репозитории есть свой URL, и этим можно воспользоваться]. Don`t forget to press "enable cjs for this host" to enable custom JS.
+Open configuration of [cjs](https://chrome.google.com/webstore/detail/custom-javascript-for-web/poakhlngfciodnhlhhgnaaelnpjljija) browser extension on the site you want to controll. Click on the link "your own external scripts", add path to file [index.js][]. Don`t forget to press "enable cjs for this host" to enable custom JS.
 
-[TODO describe what will happen after that]
+* After that, in the upper left corner of the site there will be a time counter, which you spend on viewing the current page of the site;
+* At the end of five minutes, notifications will appear with the inspirational sayings of humanity, one every 30 seconds.
 
 For faster development you can use JS code hosted on localhost. Simple web server can be used for that, run:
 
@@ -23,3 +24,5 @@ Add path `http://localhost:8000/index.js` to [cjs](https://chrome.google.com/web
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+
+[index.js]: ./index.js
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// Here You can type your custom JavaScript...
 var TIMEOUT_IN_SECS = 5 * 60
-var TEMPLATE = '<h1><span id="timer-minutes">00</span>:<span id="timer-seconds">00</span></h1>'
+var TEMPLATE = '<h1>Ты вне реальности уже <span id="timer-minutes">00</span> мин. <span id="timer-seconds">00</span> сек.</h1>'
 
 // adds HTML tag to current page
 var timerContainer = document.createElement('div')
-timerContainer.setAttribute("style", "height: 100px;")
+timerContainer.setAttribute("style", "position:fixed; background-color:black; left: 10px; top: 30px; padding: 3px; z-index: 10000;")
 var bodyTag = document.body
 bodyTag.insertBefore(timerContainer, bodyTag.firstChild)
 timerContainer.innerHTML = TEMPLATE
@@ -17,17 +18,39 @@ function padZero(number){
   return ("00" + String(number)).slice(-2);
 }
 
+var ShowQuote = function(){
+  $.ajax(
+  {
+    dataType : "jsonp",
+    jsonp : "jsonp",
+    url : "https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=ru",
+    error : function(data) {
+      alert("Error - 2");
+    },
+    success : function(response){
+      console.log(response.quoteText);
+      var quote = response.quoteText;
+      if (response.quoteAuthor !== ''){
+        quote = quote + ' (' + response.quoteAuthor + ')';
+      }
+      alert(quote);
+    }
+  });
+};
+
 var timestampOnStart = getTimestampInSecs()
 
 function displayTimer(){
   var currentTimestamp = getTimestampInSecs()
   var secsGone = currentTimestamp - timestampOnStart
-  var secsLeft = Math.max(TIMEOUT_IN_SECS - secsGone, 0)
 
-  var minutes = Math.floor(secsLeft / 60);
-  var seconds = secsLeft - minutes * 60;
-
+  var minutes = Math.floor(secsGone / 60);
+  var seconds = secsGone - minutes * 60;
   document.getElementById('timer-minutes').innerHTML = padZero(minutes)
   document.getElementById('timer-seconds').innerHTML = padZero(seconds)
+  if (secsGone >= TIMEOUT_IN_SECS && secsGone % 30 === 0) {
+      ShowQuote()
+  }
 }
-setInterval(displayTimer, 300)
+
+setInterval(displayTimer, 1000)
